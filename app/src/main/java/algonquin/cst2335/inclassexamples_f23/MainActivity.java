4df8a0d65aebe2391728c7fcf5a45a8a7e371118
Eric Torunski <etorunski@gmail.com>
2023-09-12 15:55:37 -0400

    Initial commit

---
+++ app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
@@ -0,0 +1,18 @@
+package algonquin.cst2335.inclassexamples_f23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //same as public static void main(String args[])
+    @Override //app starts here
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //this is the only function call, loads stuff onto screen
+        setContentView(R.layout.activity_main);
+    }
+}
