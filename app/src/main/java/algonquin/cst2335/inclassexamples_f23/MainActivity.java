c128b1ed620c9d4f80d022621d1f0c629e91a46b
Eric Torunski <etorunski@gmail.com>
2023-10-06 11:02:38 -0400

    saved files to disk

--- app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
@@ -3,12 +3,16 @@ package algonquin.cst2335.inclassexamples_f23;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+
+import java.io.File;
 
 import algonquin.cst2335.inclassexamples_f23.databinding.ActivityMainBinding;
 
@@ -25,14 +29,39 @@ public class MainActivity extends AppCompatActivity {
         //this is the only function call, loads stuff onto screen
         setContentView(binding.getRoot());
 
+        File mySandbox = getFilesDir();//returns where you can save files
+String path = mySandbox.getAbsolutePath();
+
+if(mySandbox.exists())
+{
+    //if yes, then open it
+}
+else {
+    //don't load it
+}
+
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+//prefs had loaded data:
+       String nameFromFile = prefs.getString("LoginName", ""); //empty in case LoginName is not there
+
+        binding.emailField.setText(nameFromFile);
+
         binding.loginButton.setOnClickListener( click->{
+            //get file editor:
+            SharedPreferences.Editor editor = prefs.edit();
             //do this when clicked:
            Intent newPage = new Intent(MainActivity.this, SecondActivity.class);
-
-           newPage.putExtra("LoginName", binding.emailField.getText().toString());
+String userInput =  binding.emailField.getText().toString();
+           newPage.putExtra("LoginName",userInput); //go to next page
            newPage.putExtra("Age", 34.567);
+            startActivity(newPage);
+            //put to disk:
+            editor.putString("LoginName", userInput);//go to disk
+            editor.putFloat("Age", 34.56f);
+            editor.apply(); //send to disk
 
-           startActivity(newPage);
+
+
             /*        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
