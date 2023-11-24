1bd37f3cc297914c112a0240cc2c9e4b85588ed9
eric <eric@eric>
2023-11-24 12:15:49 -0500

    finished toolbar

--- app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_f23/MainActivity.java
@@ -11,67 +11,135 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
 
 import algonquin.cst2335.inclassexamples_f23.databinding.ActivityMainBinding;
 
 
-/** This class is the starting point of the application
- * @author Etorunski
- * @version  1.0
- */
 public class MainActivity extends AppCompatActivity {
 
-    /** @param savedInstanceState If the activity is being re-initialized after
-     *     previously being shut down then this Bundle contains the data it most
-     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
-     */
+    RequestQueue queue = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //This part goes at the top of the onCreate function:
+        queue = Volley.newRequestQueue(this);
+
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
 
        setContentView(binding.getRoot());
 
-       binding.loginButton.setOnClickListener(click -> {
-           String userInput = binding.passwordText.getText().toString();
-           if(containsUpperAndLowerCase(userInput)){
-               binding.responseText.setText("Your password has upper and lower case");
-           }
-           else binding.responseText.setText("Your password is not complex enough");
+
+       binding.loginButton.setOnClickListener( click ->  {
+           String cityName = binding.passwordText.getText().toString();
+
+            //this goes in the button click handler:
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName) +
+           "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+
+           JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    response -> {
+                       // this is called if it worked
+
+                        try {
+                            JSONObject main = response.getJSONObject("main");
+                            double temperature = main.getDouble("temp");
+
+                            double minimum = main.getDouble("temp_min");
+                            int humidity = main.getInt("humidity");
+
+
+                            JSONArray weather = response.getJSONArray("weather");
+                            int arraySize = weather.length();
+                            for(int i = 0; i < arraySize; i++)
+                            {
+                               JSONObject obj = weather.getJSONObject(i);
+                               String iconName = obj.getString("icon");
+
+                                String imageUrl = "http://openweathermap.org/img/w/" + iconName + ".png";
+
+                                File weatherImage = new File(getFilesDir() +"/" + iconName+  ".png");
+
+                                if(weatherImage.exists()){
+                                    Bitmap theImage = BitmapFactory.decodeFile(weatherImage.getAbsolutePath());
+                                    binding.weatherImage.setImageBitmap(theImage);
+                                }
+                                else {
+                                    ImageRequest imgReq = new ImageRequest(imageUrl,
+                                            (Response.Listener<Bitmap>) bitmap -> {
+                                                // Do something with loaded bitmap...
+                                                FileOutputStream fOut = null;
+                                                try {
+                                                    fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+
+                                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                                    fOut.flush();
+                                                    fOut.close();
+
+
+                                                } catch (FileNotFoundException e) {
+                                                    e.printStackTrace();
+
+                                                } catch (IOException e) {
+                                                    throw new RuntimeException(e);
+                                                }
+                                                binding.weatherImage.setImageBitmap(bitmap);
+
+
+                                            }, 1024, 1024, ImageView.ScaleType.CENTER, null,
+
+                                            (error) -> {
+                                                int i2 = 0;
+                                            });
+
+                                    queue.add(imgReq);
+                                }
+                            }
+
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+                    },
+                   error -> {
+                       //this is called if there is an error
+                       int i = 0;
+                   });
+
+
+
+           queue.add(request); //this actually launches the request onResponse, or onErrorResponse will get called
+
        });
-
-
 
      }
 
-
-    /** This function checks str and returns true if str
-     * has an upper and lower case letter in it
-     * @param str - an unused parameter
-     *
-     * @return Returns true if str has upper and lower case, otherwise false
-     */
-    boolean containsUpperAndLowerCase( String str){
-        boolean foundUpper = false;
-        boolean foundLower = false;
-
-        for(int i = 0; i < str.length(); i++)
-        {
-            //get each character in the string:
-            char c = str.charAt(i);
-            if(Character.isLowerCase(c))
-                foundLower = true;
-            else if(Character.isUpperCase(c))
-                foundUpper = true;
-        }
-        //after looked at every character
-        return foundLower && foundUpper;
-    }
 }
